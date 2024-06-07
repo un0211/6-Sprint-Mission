cc59d93b5e4f2ae9eaf112a4b59684e68122581c
un0211 <un0211@kaist.ac.kr>
2024-06-07 22:10:51 +0900

    init: 기초 세팅
    - 필요없는 파일 및 내용 삭제
    - favicon, preview 이미지 추가
    - reset.css, global.css 추가
    - 페이지 공통의 head 태그 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>판다마켓</title>
+        <meta name="description" content="일상의 모든 물건을 거래해보세요" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:image" content="/preview.png" />
+        <meta property="og:title" content="판다 마켓" />
+        <meta
+          property="og:description"
+          content="일상의 모든 물건을 거래해보세요"
+        />
+        <meta
+          property="og:url"
+          content="https://panda-market-hw.netlify.app/"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="%PUBLIC_URL%/preview.png" />
+        <meta property="twitter:title" content="판다 마켓" />
+        <meta
+          property="twitter:description"
+          content="일상의 모든 물건을 거래해보세요"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
