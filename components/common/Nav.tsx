566816974dfc30f49797309f1f59fa86d338c417
un0211 <hwryu22@gmail.com>
2024-06-10 14:08:50 +0900

    fix: 컬러팔레트 사용 안 한 부분 수정

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -48,7 +48,8 @@ function Menu({ path, menu }: { path: string; menu: string }) {
   const router = useRouter();
 
   const linkStyle = {
-    color: path === router.pathname ? "var(--point-blue)" : "#4b5563",
+    color:
+      path === router.pathname ? "var(--point-blue)" : "var(--cool-gray600)",
   };
 
   return (
