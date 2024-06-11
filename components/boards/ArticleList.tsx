7b8cf07283608c16c0a27d96b1e550ff77a8b16f
un0211 <hwryu22@gmail.com>
2024-06-12 01:28:11 +0900

    refactor: ArticleList 컴포넌트 파일 분리 (베스트와 일반 게시글 리스트 통합)

---
+++ components/boards/ArticleList.tsx
@@ -0,0 +1,86 @@
+import { NUM_BEST_ARTICLES, Order } from "@/constants/boards";
+import useFetchData from "@/hooks/useFetchData";
+import { Articles } from "@/interfaces/Article.interface";
+import Spinner from "../common/Spinner";
+import styles from "./ArticleList.module.scss";
+import Link from "next/link";
+import { BestArticle, NormalArticle } from "./Article";
+import { useDevice } from "@/lib/DeviceContext";
+
+function ArticleList({
+  articles,
+  isLoading,
+  loadingError,
+  isBest = false,
+}: {
+  articles: Articles | undefined;
+  isLoading: boolean;
+  loadingError: Error | null;
+  isBest?: boolean;
+}) {
+  const className = isBest ? styles.best : styles.normal;
+
+  if (isLoading) {
+    return (
+      <div className={styles.spinner_container}>
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <ol className={`${styles.list} ${className}`}>
+        {articles?.list.map((article) => (
+          <li key={article.id}>
+            <Link href={`/boards/${article.id}`}>
+              {isBest ? (
+                <BestArticle article={article} />
+              ) : (
+                <NormalArticle article={article} />
+              )}
+            </Link>
+            <div className={styles.divider} />
+          </li>
+        ))}
+      </ol>
+      {loadingError && <p>{loadingError.message}</p>}
+    </>
+  );
+}
+
+export function NormalArticleList({
+  order,
+  keyword,
+}: {
+  order: Order;
+  keyword: string;
+}) {
+  const query = `?orderBy=${order}&keyword=${keyword}`;
+  const fetchedData = useFetchData<Articles>(`/articles${query}`);
+  const { data: articles, isLoading, loadingError } = fetchedData;
+
+  return (
+    <ArticleList
+      articles={articles}
+      isLoading={isLoading}
+      loadingError={loadingError}
+    />
+  );
+}
+
+export function BestArticleList() {
+  const device = useDevice();
+  const query = `?pageSize=${NUM_BEST_ARTICLES[device]}`;
+  const fetchedData = useFetchData<Articles>(`/articles${query}`);
+  const { data: articles, isLoading, loadingError } = fetchedData;
+
+  return (
+    <ArticleList
+      articles={articles}
+      isLoading={isLoading}
+      loadingError={loadingError}
+      isBest={true}
+    />
+  );
+}
--- components/boards/ArticleSection.tsx
+++ components/boards/ArticleSection.tsx
@@ -1,13 +1,10 @@
-import useFetchData from "@/hooks/useFetchData";
 import styles from "./ArticleSection.module.scss";
-import { Articles } from "@/interfaces/Article.interface";
-import Spinner from "../common/Spinner";
 import Link from "next/link";
-import { NormalArticle } from "./Article";
 import { useState } from "react";
 import { Order } from "@/constants/boards";
 import SearchBar from "../common/SearchBar";
 import Dropdown from "../common/Dropdown";
+import { NormalArticleList } from "./ArticleList";
 
 function ArticleSection() {
   const [order, setOrder] = useState(Order.Recent);
@@ -37,42 +34,6 @@ function ArticleSection() {
 
       <NormalArticleList order={order} keyword={keyword} />
     </section>
-  );
-}
-
-function NormalArticleList({
-  order,
-  keyword,
-}: {
-  order: Order;
-  keyword: string;
-}) {
-  const query = `?orderBy=${order}&keyword=${keyword}`;
-  const fetchedData = useFetchData<Articles>(`/articles${query}`);
-  const { data: articles, isLoading, loadingError } = fetchedData;
-
-  if (isLoading) {
-    return (
-      <div className={styles.spinner_container}>
-        <Spinner />
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <ol className={styles.list}>
-        {articles?.list.map((article) => (
-          <li key={article.id}>
-            <Link href={`/boards/${article.id}`}>
-              <NormalArticle article={article} />
-            </Link>
-            <div className={styles.divider} />
-          </li>
-        ))}
-      </ol>
-      {loadingError && <p>{loadingError.message}</p>}
-    </>
   );
 }
 
